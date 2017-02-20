7b85f685d50fe98e84ff517694992b6aa71cc062
alexkrzoska <alex.krzoska@gmail.com>
2017-02-20 00:20:02 -0500

    Merge origin/master
    Conflicts:
    	web/eventsmanager/eventsmanager.js

--- src/java/GoHost/service/UserFacadeREST.java
+++ src/java/GoHost/service/UserFacadeREST.java
@@ -63,7 +63,7 @@ public class UserFacadeREST extends AbstractFacade<User> {
     @GET
     @Path("{id}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public User findUser(@QueryParam("iduser") Integer id) {
+    public User findUser(@QueryParam("id") Integer id) {
         User u = null;
         u = em.createNamedQuery("User.findById", User.class).setParameter("iduser", id).getSingleResult();
         return u;
