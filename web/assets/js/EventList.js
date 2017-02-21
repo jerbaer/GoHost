6ac9dc4c0b509f6472877a1fdf7196b53cf22034
Jerry <Jerry@143.44.67.0>
2017-02-20 21:21:04 -0600

    Merge origin/master
    Conflicts:
    	web/eventsmanager/eventsmanager.js

--- web/assets/js/EventList.js
+++ web/assets/js/EventList.js
@@ -55,7 +55,7 @@ eventsList = {
         for(i=0;i<data.length;i++){
             event1 = Event;
             eventsList.events.push(event1);
-            eventsList.events[i].createFromDB(data[i].idevent, accessor);
+            eventsList.events[i].createFromDB(data[i].idevent, eventsList.accessor);
         }
     },
     getEventsList: function () {
