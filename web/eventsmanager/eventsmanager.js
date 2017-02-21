2a3512f83d44e8fffa09fffecc967a65bd8b818d
Jerry <jerry95wang@gmail.com>
2017-02-21 02:33:32 -0600

    Merge origin/master
    Conflicts:
    	web/eventsmanager/eventsmanager.js

--- web/eventsmanager/eventsmanager.js
+++ web/eventsmanager/eventsmanager.js
@@ -70,7 +70,7 @@ function getHostStrings() {
     getStringsFromEvents(eventsHosted);
     eventsFeed = $('#hosting');
     for (n = eventTitles.length - 1; n > -1; n--) {
-        url = "event/index.html";
+        url = "../event/index.html";
         newA = $('<a>').attr('href', url).text(eventTitles[n]).on('click', function () {
             window.location.href = url;
             window.location.reload(true);
