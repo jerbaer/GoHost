e72bb72c7287e2ee6a0359bc5eb346216ac09730
mahmoud <mahmoud@Mahmoud>
2017-02-24 20:43:13 -0600

    Merge origin/revised into revised
    Conflicts:
    	web/assets/js/event.js

--- web/assets/js/category.js
+++ web/assets/js/category.js
@@ -4,29 +4,27 @@
  * and open the template in the editor.
  */
 
-Category = {
-    idcategory : 0,
-    name : "",
-    coreUrl : "http://143.44.67.0:13774/GoHost/api/",
-    create: function(idcategory1){
-               Category.idcategory = idcategory1;
-               Category.retrieveName();
-    },
-    retrieveName: function(){
+function Category (idcategory1) {
+    this.idcategory = idcategory1;
+    this.name = "";
+    this.coreUrl = "http://143.44.67.0:13774/GoHost/api/";
+    this.retrieveName();
+    
+    this.retrieveName = function(){
         //get the name of the category from the database
         var url = Category.coreUrl + "category/"+Category.idcategory;
         $.getJSON(url).done(Category.nameFollowUp);
-    },
+    };
     //Don't know if this chain of functions will work. Need to learn to do
     //things in the same function for this
-    nameFollowUp : function(data){
-        Category.name = data.name;
-    },
-    getName: function(){
-        return Category.name;
-    },
+    this.nameFollowUp  = function(data){
+        this.name = data.name;
+    };
+    this.getName = function(){
+        return this.name;
+    };
 
-    getID: function(){
-        return Category.idcategory;
-    }
-};
+    this.getID = function(){
+        return this.idcategory;
+    };
+}
