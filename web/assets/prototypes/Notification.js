5ca76f85ad37fd9c9fdaa4a2a6c8427fcc5f14e1
mahmoud <mahmoud@Mahmoud>
2017-02-26 18:05:37 -0600

    Merge origin/revised into revised
    Conflicts:
    	web/assets/js/event.js

--- web/assets/prototypes/Notification.js
+++ web/assets/prototypes/Notification.js
@@ -5,14 +5,15 @@
  */
 
 function Notification(){
-    this.message = "";
-    this.read = null;
-    this.systemName = "";
     this.idnotification = 0;
-    this.owner = null;
-    this.time = 0;
-
+    this.iduser = 0;
+    this.from = 0;
+    this.idevent = 0;
+    this.status = 0;
+    this.read = null;
+    this.timestamp = 0;
     this.displayNotification = function () {
+        
         // construct notification in html (i think)
     };
     
@@ -20,7 +21,14 @@ function Notification(){
         
     };
     
-    this.create = function (message, owner, fromSystem) {
+    this.creatFromDB = function (iduser){
+        //Get request here
+    };
+    
+    this.create = function (iduser, from, idevent, timestamp) {
+        //This does not take status and read as parameters because those
+        //Should be intitalized to not accepted and not read
+        //This would have a post request
         // var notification = {member variables}
             // $.ajax({
                 // member variables
