16312a90e63f9823acad4b9eb04d3d52b656e29b
mahmoud <mahmoud@Mahmoud>
2017-02-26 18:24:04 -0600

    Merge origin/revised into revised
    Conflicts:
    	web/assets/js/event.js

--- web/assets/prototypes/Notification.js
+++ web/assets/prototypes/Notification.js
@@ -12,14 +12,7 @@ function Notification(){
     this.status = 0;
     this.read = null;
     this.timestamp = 0;
-    this.displayNotification = function () {
-        
-        // construct notification in html (i think)
-    };
     
-    this.readNotification = function () {
-        
-    };
     
     this.creatFromDB = function (iduser){
         //Get request here
@@ -33,5 +26,31 @@ function Notification(){
             // $.ajax({
                 // member variables
             // });
+    };
+    
+    
+    this.editRead = function (read) {
+        this.read = read;
+    };
+    
+    this.editStatus = function(status){
+        this.status = status;
+    };
+    
+    
+    //Copied this from event. Change everything to fit notification
+    //Also, this hasn't been debugged in event so it could be incorrect
+    this.refreshEdits = function () {
+        var event = {read: this.read, status: this.status};
+        $.ajax({
+            url: coreUrl + 'event',
+            type: 'PUT',
+            data: JSON.stringify(user),
+            contentType: 'application/json',
+            dataType: 'json'
+        });
+        //This will have a put request that updates the db with all the edits 
+        //That might have happened to the event object. We will call this 
+        //End-all function everytime an edit happens
     };
 }
