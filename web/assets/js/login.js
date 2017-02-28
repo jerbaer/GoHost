3a1c66460f0f7df1efe8c5e95c796db88706c261
Jerry <jerry95wang@gmail.com>
2017-02-28 01:32:16 -0600

    Merge origin/master
    Conflicts:
    	web/eventsmanager/index.html

--- web/assets/js/login.js
+++ web/assets/js/login.js
@@ -59,6 +59,12 @@ sys_init = {
 
     //Gonna have to get rid of this parameter since it's being passed 
     moveToHome: function () {
+        if (data !== "0"&& data !== 0) {
+            // Storing the id number of the user
+            sessionStorage.setItem('id', parseInt(data));
+        } else {
+            $('#regWarning').show();
+        }
         window.location.href = 'home/index.html#';
         sys_init.refresh;
     },
