e9462ff92e6efd44b7792916260a1c069bac517d
Jerry <jerry95wang@gmail.com>
2017-03-06 17:01:01 -0600

    Merge origin/master
    Conflicts:
    	web/eventsmanager/index.html

--- web/profile/profilepage.js
+++ web/profile/profilepage.js
@@ -118,7 +118,8 @@ function editAccount() {
 
 function deleteAccount() {
     accessor.deleteUser();
-    url = "../../index.html"
+    url = "../index.html"
+    sessionStorage.clear();
     window.location.href = url;
     window.location.reload(true);
 }
